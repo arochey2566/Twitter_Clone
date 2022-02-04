ecc7f267b59d53ddbf18d55176064586936ef0ce
Adam <aroc2566@gmail.com>
2022-02-03 21:27:12 -0500

    edit updated + title added

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -16,6 +16,7 @@ import cloudinary.uploader
 import cloudinary.api
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
